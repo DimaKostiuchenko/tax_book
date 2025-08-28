b950ca3acb9f95dfeb47b98ccbae8eb4f11a16be
dima <chozaf@gmail.com>
2025-08-29 01:05:57 +0300

    components cleaning and refactor

--- resources/js/components/year-gantt/index.ts
+++
@@ -1,1 +0,0 @@
-export { YearGantt } from './YearGantt'
--- resources/js/pages/inbox-layout.tsx
+++
@@ -1,7 +0,0 @@
-import { InboxLayout } from '@/components/app-inbox-layout'
-
-export default function InboxLayoutPage() {
-  return (
-    <InboxLayout />
-  )
-}
